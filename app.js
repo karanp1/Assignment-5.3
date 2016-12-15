f3cd4f80bfb6aaca8894e93380037b21aee304eb
karanp1 <karanp1@gmail.com>
2016-12-16 01:59:08 +0530

    Edit 1

---
+++ app.js
@@ -0,0 +1,13 @@
+var Employee = 
+{"name": "Karan Prasad",
+"age" : "28",
+"salary" : "10 Lakhs",
+"address" : {
+	"city":"New Delhi",
+	"state":"Delhi",
+	"pincode": 110021
+	
+}}
+
+console.log(Employee.name +', '+ Employee.age +', '+ Employee.salary);
+console.log(Employee.address.city +', '+ Employee.address.state +', '+ Employee.address.pincode)
